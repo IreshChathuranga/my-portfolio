b24d0f6204f46a5d2e2a2e1c91cce07f5ac2724e
IreshChathuranga <ireshchathuranga63309.com>
2025-05-24 00:18:46 +0530

    comment js line

--- js/index.js
+++ js/index.js
@@ -47,13 +47,13 @@ prev.onclick = function(){
 }
 
 
-const bg = document.querySelector('.mypic');
-const windowWidth = window.innerWidth / 5;
-const windowHeight = window.innerHeight / 5 ;
-
-bg.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX / windowWidth;
-    const mouseY = e.clientY / windowHeight;
-
-    bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-});
+// const bg = document.querySelector('.mypic');
+// const windowWidth = window.innerWidth / 5;
+// const windowHeight = window.innerHeight / 5 ;
+//
+// bg.addEventListener('mousemove', (e) => {
+//     const mouseX = e.clientX / windowWidth;
+//     const mouseY = e.clientY / windowHeight;
+//
+//     bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+// });
